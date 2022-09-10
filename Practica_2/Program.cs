0efd1f48665ce009cc895c950e0e4e1b07fd90f7
arodr <arodr@LAPTOP-G5JTKK88>
2022-09-09 23:12:36 -0700

    Agregar archivos de proyecto.

---
+++ Practica_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 23;
+            string Nombre = "Marco";
+            string Pais = "Mexico";
+
+            Console.WriteLine("Mi nombre es: {0} tengo: {1} y soy de: {2}", Nombre, edad, Pais);
+            Console.ReadKey();
+        }
+    }
+}
